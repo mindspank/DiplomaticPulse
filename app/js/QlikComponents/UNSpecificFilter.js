d98729cdadd6ef936f70b60c42e140eb2c7f3f33
mindspank <alec.karlsson@gmail.com>
2015-08-26 20:52:59 -0400

    UNSpecificFilter complete

--- app/js/QlikComponents/UNSpecificFilter.js
+++ app/js/QlikComponents/UNSpecificFilter.js
@@ -14,39 +14,45 @@
     };
 }(jQuery));
 
-function UNFilter(field, label, element, filterOn) {
+function UNFilter(container) {
 
   var list;
-  var listId;
-  var $el = element;
-  var existsInDOM = false;
-  var selectedState = false;
-  var openState = false;
-  var searchable = shouldsearch || false;
-  var labeltrim = label.replace(/\s+/g, '').replace(/\./g, '');
 
-  /**
-   * Filter HTML template
-   */
-  var tmpl = '<div id="' + labeltrim + '" class="filter">';
-  tmpl += '<div class="title">' + label;
-  tmpl += '  <div class="right"><div class="count"></div><img src="static/img/toggle.svg"></div>';
-  tmpl += '</div>';
-  tmpl += '<div class="items"></div>';
-  tmpl += '</div>';
-
-  var $div = existsInDOM ? $('#' + labeltrim) : $(tmpl);
-  var $items = $div.find('.items');
-  var $title = $div.find('.title');
-  var $count = $div.find('.count');
-
-  /**
-   * Expand Filter on click.
-   */
-  $title.on('click', function() {
-    $(this).parent().toggleClass('expanded')
-  });
-
+  var FILTERS = [{
+      label: 'Member State',
+      id: 'Member State'
+    }, 
+    { 
+      label: 'Int. Organization',
+      id: 'International Organization'
+    }, 
+    { 
+      label: 'UN Systems',
+      id: 'United Nations System'
+    }].reduce(function(d, item) {
+      
+      d[item.id] = d[item.id] || {};
+      
+      var tmpl = '<div id="' + item.id.replace(/\s+/g, '').replace(/\./g, '') + '" class="filter">';
+      tmpl += '<div class="title">' + item.label;
+      tmpl += '  <div class="right"><div class="count" style="visibility: hidden;"></div><img src="static/img/toggle.svg"></div>';
+      tmpl += '</div>';
+      tmpl += '<div class="items"></div>';
+      tmpl += '</div>';
+      
+      var $template = $(tmpl);
+      $template.find('.title').on('click', function() {
+        $(this).parent().toggleClass('expanded')
+      });
+ 
+      d[item.id].id = item.id;
+      d[item.id].dom = $template;
+      
+      $template.appendTo(container);
+ 
+      return d
+    }, {});
+    
   /**
    * Create the Qlik Sense Object
    * https://help.qlik.com/sense/2.0/en-us/developer/Subsystems/EngineAPI/Content/GenericObject/PropertyLevel/ListObjectDef.htm
@@ -62,17 +68,17 @@ function UNFilter(field, label, element, filterOn) {
       "qLibraryId": "",
       "qShowAlternatives": true,
       "qDef": {
-        "qFieldDefs": [field],
+        "qFieldDefs": ['[Entity Name]'],
         "qSortCriterias": [{
 			     "qSortByAscii": 1
 		    }]
       },
       "qExpressions": [{
-        "qExpr": "=only([Entity Type])"
+        "qExpr": "=only({1}[Entity Type])"
       }],
       "qInitialDataFetch": [{
         "qTop": 0,
-        "qHeight": 300,
+        "qHeight": 500,
         "qLeft": 0,
         "qWidth": 2
       }]
@@ -88,69 +94,66 @@ function UNFilter(field, label, element, filterOn) {
      */
     list.getLayout().then(function(layout) {
       
-      listId = layout.qInfo.qId; 
-       
-      $items.find('ul').remove();
+      listId = layout.qInfo.qId;
       
       var items = layout.qListObject.qDataPages[0].qMatrix;
-      var selected = layout.qListObject.qDimensionInfo.qStateCounts.qSelected;
+      var combinedLists = items.map(function(item) { 
+        return {
+            membertype: item[1].qText,
+            membername: item[0].qText,
+            elemNumber: item[0].qElemNumber,
+            state: item[0].qState
+          };
+        })
+        .filter(function(item) {
+          return item.membertype !== '-'
+        })
+        .reduce(function(membertype, item) {
+          
+          membertype[item.membertype] = membertype[item.membertype] || {};
+          membertype[item.membertype].selected = membertype[item.membertype].selected || 0;
+          membertype[item.membertype].items = membertype[item.membertype].items || [];
+          
+          membertype[item.membertype].items.push('<li data-elem="' + item.elemNumber + '" class="' + item.state + ' listitem"><p class="value">' + item.membername + '</p></li>')
+          membertype[item.membertype].totalitems = membertype[item.membertype].items.length;
+          
+          if(item.state === 'S') ++membertype[item.membertype].selected;
+          return membertype;
+        }, {});
+        
+        Object.keys(FILTERS).forEach(function(d) {
+          
+          FILTERS[d].dom.find('ul').remove();
 
-      items.filter(function(d) {
-        return d[1]
-      });
+          $('<ul class="list" />').html(combinedLists[d].items.join('')).appendTo(FILTERS[d].dom.find('.items'));
+          
+          if(combinedLists[d].selected != 0) {
+            FILTERS[d].dom.find('.count').visible().text(combinedLists[d].selected + ' of ' + combinedLists[d].totalitems);
+          } else {
+            FILTERS[d].dom.find('.count').invisible();
+          }
+          
+          FILTERS[d].dom.find('li:not(.x)').on('click', function() {
+            select($(this).attr('data-elem'))
+          })
+          
+          FILTERS[d].dom.appendTo(container);
 
-      /**
-       * Show/Hide selected counter based on data
-       */
-      if (selected > 0) {
-        selectedState = true;
-        $count.visible().text(selected + ' of ' + layout.qListObject.qSize.qcy);
-      } else {
-        selectedState = false;
-        $count.invisible();
-      }
-
-      /**
-       * Append list items, data-elem contains the Qlik Sense Index to be selected on the data model on click.
-       */
-      var $ul = $('<ul class="list" />');
-      $ul.html(items.map(function(d) {
-        return '<li data-elem="' + d[0].qElemNumber + '" class="' + d[0].qState + ' listitem"><p class="value">' + d[0].qText + '</p></li>'
-      }));
-
-      $ul.find('li:not(.x)').on('click', function() {
-        select($(this).attr('data-elem'))
-      })
-
-      $ul.appendTo($items);
-      if (!existsInDOM) {
-        $div.appendTo(element);
-        existsInDOM = true;
-      };
-      
-      /**
-       * If Filter should be searchable instanciate List.js
-       */
-      if(searchable) searchList();
-
-      $('input.search').val('')
+          if($('#' + FILTERS[d].id.replace(/\s+/g, '').replace(/\./g, '')).find('.search').length === 0 ) {
+            $('#' + FILTERS[d].id.replace(/\s+/g, '').replace(/\./g, '')).find('.items').before('<input class="search" placeholder="Search list"/>')
+          }
+ 
+          var s = new List(document.getElementById(FILTERS[d].id.replace(/\s+/g, '').replace(/\./g, '')), {
+            valueNames: ['value']
+          });         
+          
+          $('input.search').val('')
+          
+        });
 
     });
   };
-
-  /**
-   * Set up searchable lists using List.js
-   */
-  function searchList() {
-    if($('#' + labeltrim).find('.search').length === 0 ) {
-      $('#' + labeltrim).find('.items').before('<input class="search" placeholder="Search list"/>')
-    }
-
-    var s = new List(document.getElementById(labeltrim), {
-      valueNames: ['value']
-    });
-  };
-
+  
   /**
    * Select a value in the Qlik Sense Data Model.
    * Will trigger a update message to notify other objects to update accordingly.
